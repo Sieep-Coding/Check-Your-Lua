9a1938420b03487c06162e55863e86a474d8b32f
sieep-coding <nickstambaugh@proton.me>
2026-05-17 12:55:53 -0400

    pass test

--- experimental/stack.lua
+++ experimental/stack.lua
@@ -1,3 +1,14 @@
 local Stack = {}
 
+Stack.__index = Stack
+
+function Stack.new()
+    local self = setmetatable({}, Stack)
+    return self
+end
+
+function Stack:size()
+    return 0
+end
+
 return Stack
